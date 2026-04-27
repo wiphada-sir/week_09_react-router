b9725746c3c8eac49c8ce2e165e16506e9f73253
wiphada-sir <wiphada.sir@gmail.com>
2026-04-27 11:56:49 +0700

    modified Home.jsx

--- react-router/src/pages/Home.jsx
+++ react-router/src/pages/Home.jsx
@@ -7,12 +7,12 @@ export default function Home() {
         navigate("/about");
     };
     
-
     return (
         <div className="text-center">
             <h1 className="text-3xl font-bold mb-4">Home Page</h1>
             <button className="px-4 py-2 bg-teal-500 text-white rounded-md hover:bg-teal-700"
-            onClick={handleNavigate}>Go to About</button>
+            onClick={handleNavigate}>Go to About
+            </button>
             <Link to="/about">Go to About</Link>
         </div>
     );
